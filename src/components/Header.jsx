243cabbc9d0d5f0274f24d481916d91219c2ebeb
DiegoDeveloper7 <diegoag8@gmail.com>
2025-06-22 15:38:43 -0400

    Inicio desafio pizzeria - componentes

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import '../assets/estilos/estilos.css';
+
+export const Header = () => {
+  return (
+    <div className="header-bg text-white text-center py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="display-3 fw-bold mb-3">
+              ¡Pizzería MammaMia!
+            </h1>
+            <p className="lead fs-4">
+              ¡Tenemos las mejores pizzas que podrás encontrar!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import { TarjetaPizza } from "./tarjetaPizza"
+import napolitanaImg from '../assets/imgs/napolitana.jpg';
+import españolaImg from '../assets/imgs/española.jpg';
+import peperoniImg from '../assets/imgs/peperoni.webp';
+
+export const Home = () => {
+  return (
+    <>
+      <div className="container my-5">
+        <div className="row g-4">
+          <div className="col-md-4">
+            <TarjetaPizza
+              name="Pizza Napolitana"
+              price={5950}
+              ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
+              img={napolitanaImg}
+            />
+          </div>
+          <div className="col-md-4">
+            <TarjetaPizza
+              name="Pizza Española"
+              price={6950}
+              ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone", "aceitunas"]}
+              img={españolaImg}
+            />
+          </div>
+          <div className="col-md-4">
+            <TarjetaPizza
+              name="Pizza Pepperoni"
+              price={6950}
+              ingredients={["mozzarella", "pepperoni", "orégano", "pimienta negra"]}
+              img={peperoniImg}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
