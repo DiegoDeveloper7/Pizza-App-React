181fe16a712241c39a5b64722f3dce0bc488c050
DiegoDeveloper7 <diegoag8@gmail.com>
2025-06-23 11:09:11 -0400

    4to modificacion componente tarjetaPizza estilo y forma

--- src/components/TarjetaPizza.jsx
+++ src/components/TarjetaPizza.jsx
@@ -2,6 +2,7 @@
 export const TarjetaPizza = ({ img, name, price, ingredients }) => {
     
   return (
+
   <div className="card h-100 shadow border-0">
   <img src={img} className="card-img-top" alt={name} />
   <div className="card-body d-flex flex-column justify-content-between">
@@ -13,10 +14,8 @@ export const TarjetaPizza = ({ img, name, price, ingredients }) => {
       <p className="mb-2 ingredients-title">Ingredientes:</p>
       <div className="d-flex flex-wrap justify-content-center gap-1 list-ingredients">
         {ingredients.map((ingredient, index) => (
-          <span key={index} className="badge px-3 py-2 text-capitalize text-primary fs-5
-
-">
-            <i className="fas fa-leaf me-1" style={{ fontSize: "0.7em" }}></i> {ingredient}
+          <span key={index} className="badge px-3 py-2 text-capitalize text-primary fs-6">
+            <i className="fas fa-leaf me-1" style={{ fontSize: "0.9em" }}></i> {ingredient}
           </span>
         ))}
       </div>
@@ -30,16 +29,18 @@ export const TarjetaPizza = ({ img, name, price, ingredients }) => {
     </div>
 
     {/* Botones */}
-    <div className="d-flex justify-content-between mt-auto gap-3 btn-options">
-      <button className="btn flex-fill py-2 see-more">
-        <i className="fas fa-eye me-2"></i> Ver más
-      </button>
-      <button className="btn flex-fill py-2 add-to-cart">
-        <i className="fas fa-cart-plus me-2"></i> Añadir
-      </button>
-    </div>
+
+   <div className="d-flex justify-content-between mt-auto gap-3 btn-options">
+  <button className="btn btn-warning border-warning text-dark flex-fill py-2 see-more">
+    <i className="fas fa-eye me-2"></i> Ver más
+  </button>
+  <button className="btn btn-success border-success text-white flex-fill py-2 add-to-cart">
+    <i className="fas fa-cart-plus me-2"></i> Añadir
+  </button>
+   </div>
 
   </div>
 </div>
+
   )
 }
